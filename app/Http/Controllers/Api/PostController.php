32e03ce6b7d1de050d6a02d7e824bfbe10b64b23
rholanddeo <rholanddeo210@gmail.com>
2024-03-23 09:25:36 +0700

    make update

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
@@ -41,5 +42,43 @@ class PostController extends Controller
         ]);
 
         return new PostResource(true, 'Post created successfully', $post);
+    }
+
+    public function show(Post $post)
+    {
+        return new PostResource(true, 'Data retrieved successfully', $post);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $validation = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json($validation->errors(), 422);
+        }
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+            Storage::delete('public/posts/' . $post->image);
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+                'slug' => Str::slug($request->title),
+                'image' => $image->hashName(),
+            ]);
+        } else {
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+                'slug' => Str::slug($request->title),
+            ]);
+        }
+
+        return new PostResource(true, 'Post updated successfully', $post);
     }
 }
